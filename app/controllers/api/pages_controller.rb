5e737f6d2820d403aca6f2259fcbcc7724f1b815
Andrew Basner <andrewbasner@gmail.com>
2018-06-25 19:22:20 -0400

    clean up methods in controller again

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,9 +1,9 @@
 class Api::PagesController < ApplicationController
 
   def random_fortune_action
-    which_fortune = rand(0..2)
+    # which_fortune = rand(0..2)
     fortunes = ["A friend asks only for your time, not your money.", "If you refuse to accept anything but the best, you very often get it.", "A smile is your passport into the hearts of others."]
-    @fortune = fortunes[which_fortune]
+    @fortune = fortunes.sample
     
     render "fortune_view.json.jbuilder"  
   end
@@ -13,7 +13,7 @@ class Api::PagesController < ApplicationController
     6.times do
       @lotto_numbers << rand(1..60)
     end
-    
+
     render "lotto_view.json.jbuilder"
   end
 end
